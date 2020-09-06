65a4c1b327c72b5da8fcf5e0a69cb7e37a03aeaa
ibreschi <iacopobreschi@gmail.com>
2020-09-06 15:51:13 +0200

    Displaying different types of graphs

--- app/app.py
+++ app/app.py
@@ -4,6 +4,8 @@ import hashlib
 import redis
 import html
 
+import data
+
 app = Flask(__name__)
 
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
@@ -16,7 +18,14 @@ def index():
     # 1) Fetch the input each one has (metadata and data)
     # 2) for each input use the metadata to choose how to display each data
     # 3) add to the template all the input with way of displaying it
-    return render_template("index.html")
+
+    d = data.get_apple_stock_price()
+    d2 = data.get_amazon_stock_price()
+
+    items = []
+    items.append(d)
+    items.append(d2)
+    return render_template("index.html", len=len(items), items=items)
 #
 #     name = default_name
 #     if request.method == 'POST':
---
+++ app/data.py
@@ -0,0 +1,29 @@
+import csv
+import requests
+
+
+def get_data():
+    RESULT = {}
+    with open('/app/data/aapl.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        # Date,Volume,Close,Average
+        for row in reader:
+            for column, value in row.items():
+                RESULT.setdefault(column, []).append(value)
+    return RESULT
+
+
+def get_apple_stock_price():
+    data = get_data()
+    metadata = { "graph_type": "A",
+                 "style": "border:1px solid green; "}
+    data["metadata"] = metadata
+    return data
+
+
+def get_amazon_stock_price():
+    data = get_data()
+    metadata = { "graph_type": "B",
+                 "style": "border:2px solid red; "}
+    data["metadata"] = metadata
+    return data
